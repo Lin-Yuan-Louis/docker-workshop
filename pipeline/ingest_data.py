254e8f8e5a32f6953af3fe11448068884bdb7100
Lin-Yuan-Louis <97973658+Lin-Yuan-Louis@users.noreply.github.com>
2026-02-01 21:06:27 +0000

    pipeline code working

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
+url
+
+
+# In[3]:
+
+
+df = pd.read_csv(url)
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[5]:
+
+
+len(df)
+
+
+# In[6]:
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+df = pd.read_csv(
+    prefix + 'yellow_tripdata_2021-01.csv.gz',
+    nrows=100,
+    dtype=dtype,
+    parse_dates=parse_dates
+)
+
+
+# In[7]:
+
+
+len(df)
+
+
+# In[8]:
+
+
+df['tpep_dropoff_datetime']
+
+
+# In[9]:
+
+
+df.head()
+
+
+# In[10]:
+
+
+get_ipython().system('uv add sqlalchemy psycopg2-binary')
+
+
+# In[12]:
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+
+# In[16]:
+
+
+df.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists= 'replace')
+
+
+# In[14]:
+
+
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+
+
+# In[17]:
+
+
+df_iter = pd.read_csv(
+    url,
+    dtype=dtype,
+    parse_dates=parse_dates,
+    iterator = True,
+    chunksize = 100000,
+)
+
+
+# In[20]:
+
+
+get_ipython().system('uv add tqdm')
+
+
+# In[21]:
+
+
+from tqdm.auto import tqdm
+
+
+# In[23]:
+
+
+for df_chunk in tqdm(df_iter):
+    df_chunk.to_sql(name='yellow_taxi_data', con = engine, if_exists='append')
+
+
+# In[18]:
+
+
+df_iter
+
+
+# In[ ]:
+
+
+
+
